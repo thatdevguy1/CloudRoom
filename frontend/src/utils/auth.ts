55bdf44b54d1031bfe7a2b17a07e2b91843d8953
David Bland <davidbland@Davids-MBP.local>
2025-01-09 13:21:56 -0500

    Fix hardcoded redirect value to use env variable

--- frontend/src/utils/auth.ts
+++ frontend/src/utils/auth.ts
@@ -1,6 +1,6 @@
 export const signUpRedirect = () => {
   const clientId = "2qc9uch823amu97rhd8r1tcvpa";
-  const signUpUri = "http://localhost:5173/callback";
+  const signUpUri = import.meta.env.VITE_REDIRECT_URI;
   const cognitoDomain = import.meta.env.VITE_COGNITO_DOMAIN;
   window.location.href = `${cognitoDomain}/signup?client_id=${clientId}&code_challenge=OQSY9NXNOZrGZUUPsfnuvLMiY5M4Bx0yQ-DMqHbycvE&code_challenge_method=S256&redirect_uri=${encodeURIComponent(
     signUpUri
